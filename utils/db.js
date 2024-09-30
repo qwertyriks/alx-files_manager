7df670c673f7888c907f5c0f65876f8bf2d54af4
qwertyriks <romeokumatse@gmail.com>
2024-09-30 21:23:26 +0000

    db In progress

--- utils/db.js
+++ utils/db.js
@@ -30,7 +30,7 @@ class DBClient {
   }
 
   /**
-   * Collects the number of users in the database.
+   * Retrieves the number of users in the database.
    * @returns {Promise<Number>}
    */
   async nbUsers() {
@@ -38,7 +38,7 @@ class DBClient {
   }
 
   /**
-   * Collects the number of files in the database.
+   * Retrieves the number of files in the database.
    * @returns {Promise<Number>}
    */
   async nbFiles() {
@@ -46,7 +46,7 @@ class DBClient {
   }
 
   /**
-   * Collects a reference to the `users` collection.
+   * Retrieves a reference to the `users` collection.
    * @returns {Promise<Collection>}
    */
   async usersCollection() {
@@ -54,7 +54,7 @@ class DBClient {
   }
 
   /**
-   * Collects a reference to the `files` collection.
+   * Retrieves a reference to the `files` collection.
    * @returns {Promise<Collection>}
    */
   async filesCollection() {
